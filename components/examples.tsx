f31f551ae029b0bb3da629cccb8dad906e3ce30c
Francis <144542168+Franch62@users.noreply.github.com>
2025-01-25 17:00:30 -0300

    home page to component and fix height

--- components/examples.tsx
+++ components/examples.tsx
@@ -5,11 +5,11 @@ export default function Examples() {
         <section className="rounded-md flex justify-center text-gray-600 body-font">
             <div className="container max-w-screen-lg py-4 flex items-center justify-around flex-wrap lg:flex-nowrap ">
                 <div className="lg:w-1/2 flex flex-col lg:items-start items-center justify-center text-center lg:text-left md:mx-6 max-w-lg">
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium font-bold text-slate-600 dark:text-gray-200">
                         See the Power of Custom
                         <br className="hidden lg:inline-block" /> QR Codes in Action
                     </h1>
-                    <p className="mb-8 leading-relaxed">
+                    <p className="mb-8 leading-relaxed ">
                         Personalize every detail to make your QR codes stand out. Perfect for your business, events, or personal touch.
                     </p>
                     <div className="flex justify-center lg:justify-start">
@@ -20,15 +20,14 @@ export default function Examples() {
                 </div>
 
                 <div className="h-[26rem] w-full flex items-center justify-center ">
-
                     <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
-                        <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+                        <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-600 dark:text-gray-200">
                             Your Custom QR Code
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
-                            <span className="text-slate-500 ">
+                            <p>
                                 Customize with your logo and share with style.
-                            </span>
+                            </p>
                         </div>
                         <Image
                             src="/image.jpg"
