a2b1291f53d7ee765d1c8615dd83b8fdaebf87a5
Fernando Divino de Moraes Júnior <kinggprobr@gmail.com>
2025-01-25 12:42:11 -0300

    comentando o pinContainer

--- components/examples.tsx
+++ components/examples.tsx
@@ -17,7 +17,9 @@ export default function Examples() {
                 </div>
 
                 <div className="h-[40rem] w-full flex items-center justify-center ">
-                    {/* <PinContainer title="/your-url-example.com" href="https://fernaandojr.vercel.app/">
+                    {/* // eu deixei comentado pq tava dando erro, ai depois precisa pegar pra analisar o erro
+						
+					<PinContainer title="/your-url-example.com" href="https://fernaandojr.vercel.app/">
                         <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                             <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">Your Custom QR Code</h3>
                             <div className="text-base !m-0 !p-0 font-normal">
