f4c9cbcfdb1b27b3df6766dab4373ecfbcd15c66
Francis <144542168+Franch62@users.noreply.github.com>
2025-01-25 17:07:59 -0300

    try resolve

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,17 +1,16 @@
 import SignupFormDemo from "../../components/signup-form-demo"
-import {BackgroundBeams} from "../../components/ui/background-beams"
+import { BackgroundBeams } from "../../components/ui/background-beams"
 
 export default function Home() {
-
-  return (
-    <div className="register-form flex justify-center mt-4 mb-4 bg-dark">
-        <form action="">
-          <div className="title flex justify-center">
-            <h1 className="text-4xl text-bold">Register</h1>
-          </div>
-          <SignupFormDemo />
-        </form>
-        <BackgroundBeams />
-      </div>
-  );
+    return (
+        <div className="register-form flex justify-center mt-4 mb-4 bg-dark">
+            <form action="">
+                <div className="title flex justify-center">
+                    <h1 className="text-4xl text-bold">Register</h1>
+                </div>
+                <SignupFormDemo />
+            </form>
+            <BackgroundBeams />
+        </div>
+    )
 }
