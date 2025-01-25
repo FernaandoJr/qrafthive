39b712e8744a972e2d4ec89ddb01006bbd14c333
Francis <144542168+Franch62@users.noreply.github.com>
2025-01-25 17:31:34 -0300

    Commit forçado 3

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import { BackgroundBeams } from "../components/ui/background-beams"
 
-
 export function Hero() {
     return (
         <div className="h-600px flex md:min-h-[80vh] flex-col w-full gap-4 items-center justify-center px-4">
