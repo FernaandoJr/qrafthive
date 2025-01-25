001e8e3b7caafd363d1a0ac5126345c180a43982
Francis <144542168+Franch62@users.noreply.github.com>
2025-01-25 15:03:08 -0300

    Home page changes

--- components/examples.tsx
+++ components/examples.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image'
-import { PinContainer } from "./ui/3d-pin";
 
 export default function Examples() {
     return (
         <section className="rounded-md flex justify-center text-gray-600 body-font">
             <div className="container max-w-screen-lg py-4 flex items-center justify-around flex-wrap lg:flex-nowrap ">
-                    <div className="lg:w-1/2 flex flex-col lg:items-start items-center justify-center text-center lg:text-left md:mx-6 max-w-lg">
+                <div className="lg:w-1/2 flex flex-col lg:items-start items-center justify-center text-center lg:text-left md:mx-6 max-w-lg">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                         See the Power of Custom
                         <br className="hidden lg:inline-block" /> QR Codes in Action
@@ -14,38 +13,34 @@ export default function Examples() {
                         Personalize every detail to make your QR codes stand out. Perfect for your business, events, or personal touch.
                     </p>
                     <div className="flex justify-center lg:justify-start">
-                        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                        Customize Yours Now
+                        <button className="inline-flex text-white bg-slate-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                            Customize Yours Now
                         </button>
                     </div>
-                    </div>
+                </div>
 
-                    <div className="h-[40rem] w-full flex items-center justify-center ">
-                    <PinContainer
-                        title="/your-url-example.com"
-                        href="https://fernaandojr.vercel.app/"
-                    >
-                        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+                <div className="h-[26rem] w-full flex items-center justify-center ">
+
+                    <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
                             Your Custom QR Code
                         </h3>
                         <div className="text-base !m-0 !p-0 font-normal">
                             <span className="text-slate-500 ">
-                            Customize with your logo and share with style.
+                                Customize with your logo and share with style.
                             </span>
                         </div>
                         <Image
-                        src="/image.jpg"
-                        width={400}
-                        height={400}
-                        alt="QR Code Example"
-                        className="rounded-lg shadow-lg"
-                    />
-                        </div>
-                    </PinContainer>
+                            src="/image.jpg"
+                            width={400}
+                            height={400}
+                            alt="QR Code Example"
+                            className="rounded-lg shadow-lg"
+                        />
                     </div>
+                </div>
             </div>
-    </section>
+        </section>
 
 
     )
---
+++ components/statistics-trip.tsx
@@ -0,0 +1,32 @@
+interface Stat {
+    value: string;
+    label: string;
+}
+
+interface StatisticsTripProps {
+    stats: Stat[];
+}
+
+export function StatisticsTrip({ stats }: StatisticsTripProps) {
+    return (
+        <div className="p-4 md:p-6 mt-6 text-white bg-zinc-900 w-[110%] overflow-hidden">
+            <div className="flex flex-wrap justify-around items-center">
+                {stats.map((stat, index) => (
+                    <div key={index} className="w-full md:w-1/2 lg:w-1/4 py-8">
+                        <div
+                            className={`px-12 ${index !== stats.length - 1
+                                ? 'lg:border-r md:border-r border-gray-200'
+                                : ''
+                                }`}
+                        >
+                            <h2 className="text-4xl text-slate-500 md:text-5xl font-semibold text-center">
+                                {stat.value}
+                            </h2>
+                            <p className="mb-2 text-center">{stat.label}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
