aac902816fe14686572ef279c8f945acb806f964
Francis <144542168+Franch62@users.noreply.github.com>
2025-01-26 16:47:25 -0300

    reformulando Login page

--- components/signin-form-demo.tsx
+++ components/signin-form-demo.tsx
@@ -8,7 +8,7 @@ import {
   IconBrandGoogle,
 } from "@tabler/icons-react";
 
-export default function SignupFormDemo() {
+export default function SignInFormDemo() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form submitted");
@@ -16,19 +16,9 @@ export default function SignupFormDemo() {
   return (
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 mt-4 md:p-8 shadow-input bg-white dark:bg-black">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">Welcome to QRaftHive</h2>
-      <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">Register and have an adventure with your creations</p>
+      <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">Log-in to live an unforgettable experience.</p>
 
       <form className="my-8" onSubmit={handleSubmit}>
-        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-          <LabelInputContainer>
-            <Label htmlFor="firstname">First name</Label>
-            <Input id="firstname" placeholder="ex: John" type="text" />
-          </LabelInputContainer>
-          <LabelInputContainer>
-            <Label htmlFor="lastname">Last name</Label>
-            <Input id="lastname" placeholder="ex: Doe" type="text" />
-          </LabelInputContainer>
-        </div>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
           <Input id="email" placeholder="example@gmail.com" type="email" />
@@ -39,7 +29,7 @@ export default function SignupFormDemo() {
         </LabelInputContainer>
 
         <button className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]" type="submit">
-          Sign up &rarr;
+          Log in &rarr;
           <BottomGradient />
         </button>
 
