7e2a73990fdaeca4e34342a2c1616a4c05d6eb99
Fernando Divino de Moraes Junior <kinggprobr@gmail.com>
2025-01-27 15:43:12 -0300

    Melhorando a navbar pra celulares

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import { motion } from "framer-motion"
 import { Menu, MoveRight, X } from "lucide-react";
 import { useState } from "react";
 import { ModeToggle } from "./theme-menu";
@@ -54,7 +55,7 @@ function Header1() {
     const [isOpen, setOpen] = useState(false)
     return (
         <header className="w-full z-40 sticky top-0 left-0 bg-background">
-            <div className="container relative mx-auto min-h-20 flex gap-4 flex-row lg:grid lg:grid-cols-3 items-center">
+            <div className="container relative mx-auto min-h-20 flex gap-4 flex-row lg:grid lg:grid-cols-3 items-center ">
                 <div className="justify-start items-center gap-4 lg:flex hidden flex-row">
                     <NavigationMenu className="flex justify-start items-start">
                         <NavigationMenuList className="flex justify-start gap-4 flex-row">
@@ -99,7 +100,6 @@ function Header1() {
                 </div>
                 <div className="flex justify-end w-full gap-4">
                     <Button variant="outline">Sign in</Button>
-                    <Button>Get started</Button>
                     <ModeToggle />
                 </div>
                 <div className="flex w-12 shrink lg:hidden items-end justify-end">
@@ -107,23 +107,29 @@ function Header1() {
                         {isOpen ? <X className="w-5 h-5" /> : <Menu className="w-5 h-5" />}
                     </Button>
                     {isOpen && (
-                        <div className="absolute top-20 border-t flex flex-col w-full right-0 bg-background shadow-lg py-4 container gap-8">
+                        <div className="absolute top-20 mt-2 border flex flex-col w-full right-0 bg-background shadow-lg p-4 container border-border rounded-lg gap-5 ">
                             {navigationItems.map((item) => (
                                 <div key={item.title}>
                                     <div className="flex flex-col gap-2">
                                         {item.href ? (
                                             <Link href={item.href} className="flex justify-between items-center">
                                                 <span className="text-lg">{item.title}</span>
-                                                <MoveRight className="w-4 h-4 stroke-1 text-muted-foreground" />
+                                                <MoveRight className="w-4 h-4 stroke-1 text-muted-foreground " />
                                             </Link>
                                         ) : (
-                                            <p className="text-lg">{item.title}</p>
+                                            <motion.div whileHover={{ scale: 1.05, zIndex: 1 }} className="w-fit">
+                                                <p className="text-lg">{item.title}</p>
+                                            </motion.div>
                                         )}
                                         {item.items &&
                                             item.items.map((subItem) => (
                                                 <Link key={subItem.title} href={subItem.href} className="flex justify-between items-center">
-                                                    <span className="text-muted-foreground">{subItem.title}</span>
-                                                    <MoveRight className="w-4 h-4 stroke-1" />
+                                                    <motion.div whileTap={{ scale: 0.9 }} whileHover={{ scale: 1.05, zIndex: 1 }}>
+                                                        <span className="text-muted-foreground">{subItem.title}</span>
+                                                    </motion.div>
+                                                    <motion.div whileTap={{ scale: 0.9 }} whileHover={{ scale: 1.1, zIndex: 1 }}>
+                                                        <MoveRight className="w-4 h-4 stroke-1" />
+                                                    </motion.div>
                                                 </Link>
                                             ))}
                                     </div>
