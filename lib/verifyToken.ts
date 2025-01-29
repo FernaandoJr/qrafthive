c80fecc6b7087044bc22a824c669a6c3f2795946
Francis <28francis.junior@gmail.com>
2025-01-29 19:31:34 -0300

    arrumando deploy

--- lib/verifyToken.ts
+++ lib/verifyToken.ts
@@ -1,6 +1,11 @@
 import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
+interface DecodedToken {
+    userId: string;
+    role: string;
+}
+
 export const verifyToken = async (req: Request) => {
     const token = req.headers.get("Authorization")?.split(" ")[1];
     if (!token) {
@@ -8,9 +13,9 @@ export const verifyToken = async (req: Request) => {
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-        (req as any).user = decoded;
-    } catch (err) {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as DecodedToken;
+        (req as Request & { user?: DecodedToken }).user = decoded;
+    } catch {
         return NextResponse.redirect("/login");
     }
 
