50881da112bdd2249077b9d63b6240cdcb5b5b80
Fernando Divino de Moraes Júnior <kinggprobr@gmail.com>
2025-12-18 11:09:05 -0300

    refactor: server index and update TypeScript configuration

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,10 +1,10 @@
-import "dotenv/config";
-import { Elysia } from "elysia";
-import { cors } from "@elysiajs/cors";
-import { createContext } from "@qrafttt/api/context";
-import { appRouter } from "@qrafttt/api/routers/index";
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { auth } from "@qrafttt/auth";
+import { cors } from "@elysiajs/cors"
+import { createContext } from "@qrafttt/api/context"
+import { appRouter } from "@qrafttt/api/routers/index"
+import { auth } from "@qrafttt/auth"
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch"
+import "dotenv/config"
+import { Elysia } from "elysia"
 
 const app = new Elysia()
 	.use(
@@ -13,14 +13,14 @@ const app = new Elysia()
 			methods: ["GET", "POST", "OPTIONS"],
 			allowedHeaders: ["Content-Type", "Authorization"],
 			credentials: true,
-		}),
+		})
 	)
 	.all("/api/auth/*", async (context) => {
-		const { request, status } = context;
+		const { request, status } = context
 		if (["POST", "GET"].includes(request.method)) {
-			return auth.handler(request);
+			return auth.handler(request)
 		}
-		return status(405);
+		return status(405)
 	})
 	.all("/trpc/*", async (context) => {
 		const res = await fetchRequestHandler({
@@ -28,10 +28,10 @@ const app = new Elysia()
 			router: appRouter,
 			req: context.request,
 			createContext: () => createContext({ context }),
-		});
-		return res;
+		})
+		return res
 	})
 	.get("/", () => "OK")
-	.listen(3000, () => {
-		console.log("Server is running on http://localhost:3000");
-	});
+	.listen(9876, () => {
+		console.log("Server is running on http://localhost:9876")
+	})
